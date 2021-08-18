b753f467d75c1fee4d2801685f3e83c472f1c447
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-08-17 23:17:56 -0300

    img, json, html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="logo">
+        <img src="/static/images/super-shoes.png" alt="logo"/>
+      </div>
+      <div className="carrossel">
+        <div className="item">
+          <div className="image">
+            <img src="https://imgcentauro-a.akamaihd.net/230x230/94313731.jpg" alt="shoe"/>
+          </div>
+          <div className="info">
+            <span className="name">Super Shoes 1</span>
+            <span className="oldPrice">U$ 599,00</span>
+            <span className="price">U$ 199,00</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
